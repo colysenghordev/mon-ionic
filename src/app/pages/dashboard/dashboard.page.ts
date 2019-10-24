71825b09d5d69ba15b0f7cbd4445e34bd8fa4675
colysenghordev <teng2mail@yahoo.com>
2019-10-24 17:52:19 +0000

    création formulaire

--- src/app/pages/dashboard/dashboard.page.ts
+++ src/app/pages/dashboard/dashboard.page.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,13 +8,73 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./dashboard.page.scss'],
 })
 export class DashboardPage implements OnInit {
+  FormGroup
+  @ViewChild('signupSlider') signupSlider;
 
-  constructor(private authService: AuthService) { }
+  public slideOneForm: FormGroup;
+  public slideTwoForm: FormGroup;
+  public slideThreeForm: FormGroup;
+
+  public submitAttempt: boolean = false;
+
+
+  constructor(private authService: AuthService, public formBuilder: FormBuilder) {
+
+    this.slideOneForm = formBuilder.group({
+      prenom: ['', Validators.compose([Validators.maxLength(30), Validators.minLength(2), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+      nom: ['', Validators.compose([Validators.maxLength(30), Validators.minLength(2), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+      telephone: ['', Validators.required,],
+      cni: ['', Validators.required]
+    });
+
+    this.slideTwoForm = formBuilder.group({
+      prenom: ['', Validators.compose([Validators.maxLength(30), Validators.minLength(2), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+      nom: ['', Validators.compose([Validators.maxLength(30), Validators.minLength(2), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+      telephone: ['', Validators.required,],
+      cni: ['', Validators.required]
+    });
+
+    this.slideThreeForm = formBuilder.group({
+      montant: ['', Validators.required],
+      frais: ['', Validators.required],
+    });
+
+  }
 
   ngOnInit() {
   }
 
-  logOut(){
+  logOut() {
     return this.authService.logOut();
+  }
+
+  next() {
+    this.signupSlider.slideNext();
+  }
+
+  prev() {
+    this.signupSlider.slidePrev();
+  }
+
+  save() {
+
+    this.submitAttempt = true;
+
+    if (!this.slideOneForm.valid) {
+      this.signupSlider.slideTo(0);
+    }
+    else if (!this.slideTwoForm.valid) {
+      this.signupSlider.slideTo(1);
+    }
+    else if (!this.slideThreeForm.valid) {
+      this.signupSlider.slideTo(2);
+    }
+    else {
+      console.log("success!")
+      console.log(this.slideOneForm.value);
+      console.log(this.slideTwoForm.value);
+      console.log(this.slideThreeForm.value)
+    }
+
   }
 }
